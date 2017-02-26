2a4d8bf4ed9d9e439a61a7618a1de4f8c79a173f
Bart Wijnants <bartwijnants@outlook.com>
2017-02-26 12:35:29 +0100

    list proptypes

--- demo4/src/components/List.jsx
+++ demo4/src/components/List.jsx
@@ -3,7 +3,7 @@ import ListItem from './ListItem';
 
 export default class List extends React.Component {
     render() {
-        const listItems = this.props.slides ? this.props.slides.map(slide => <ListItem key={slide.id} slide={slide} onSelect={this.props.onSelect} />) : [];
+        const listItems = this.props.slides.map(slide => <ListItem key={slide.id} slide={slide} onSelect={this.props.onSelect} />);
         return (
             <div id="list">
                 <ul>
@@ -13,3 +13,11 @@ export default class List extends React.Component {
         );
     }
 }
+
+List.propTypes = {
+    slides: React.PropTypes.arrayOf(React.PropTypes.shape({
+        id: React.PropTypes.number,
+        content: React.PropTypes.string
+    })).isRequired,
+    onSelect: React.PropTypes.func.isRequired
+};
